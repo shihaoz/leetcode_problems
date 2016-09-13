99124590a6dc92662a2a57cd4124c53ab04cb145
DavidZhang <shihaoz@umich.edu>
2016-09-13 10:32:21 -0400

    opt complete; 79ms

--- leetcode_max_size_sum_k.cpp
+++ leetcode_max_size_sum_k.cpp
@@ -15,23 +15,23 @@ public:
         if (nums.empty()) {
             return 0;
         }
-        int max_length = 0, sum = 0;
-        unordered_map<int, int> _lookup; // <pre_sum, index>
-        _lookup[0] = -1;    // sum = zero when nothing is present
-        vector<int> _pre_sum;
+        int max_length = 0;
+        
+        // <pre_sum, index>, sum = zero when nothing is present
+        unordered_map<int, int> _lookup; _lookup[0] = -1;
+        vector<int> _pre_sum(1, 0);
         
         for (int i = 0; i < nums.size(); i++) {
             // update sum
-            _pre_sum.push_back(sum + nums[i]);
-            sum += nums[i];
+            _pre_sum.push_back(_pre_sum.back() + nums[i]);
             
             // update lookup
-            if (_lookup.find(_pre_sum[i]) == _lookup.end()) {
-                _lookup[_pre_sum[i]] = i;
+            if (_lookup.find(_pre_sum.back()) == _lookup.end()) {
+                _lookup[_pre_sum.back()] = i;
             }
             // find if complement exists
-            if (_lookup.find(_pre_sum[i] - k) != _lookup.end()) {
-                max_length = max(max_length, i - _lookup[_pre_sum[i] - k]);
+            if (_lookup.find(_pre_sum.back() - k) != _lookup.end()) {
+                max_length = max(max_length, i - _lookup[_pre_sum.back() - k]);
             }
         }
         return max_length;
