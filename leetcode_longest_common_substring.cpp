1296bf5b90c15e54957f45975964e51180b2782b
DavidZhang <shihaoz@umich.edu>
2016-09-15 13:41:01 -0400

    word break

--- leetcode_longest_common_substring.cpp
+++ leetcode_longest_common_substring.cpp
@@ -22,6 +22,8 @@ public:
     int longestCommonSubSequence(string a, string b);
     int longestPalindromicSubsequence(string a);
     int longestIncreasingSubsequence(vector<int>& vec);
+    int editDistance(string a, string b);
+    bool wordBreak(string target, vector<string>& words);
 };
 int Solution::longestCommonSubString(string a, string b) {
     // clean edge
@@ -141,6 +143,57 @@ int Solution::longestIncreasingSubsequence(vector<int> &vec){
     return max_length;
 }
 
+int Solution::editDistance(string a, string b){
+    if (a.empty() or b.empty()) {
+        return max(a.length(), b.length());
+    }
+    // setup
+    int min_distance = numeric_limits<int>::max();
+    vector<vector<int>> _states(a.size()+1, vector<int>(b.size()+1, 0));
+    /// pad outside, where b is empty
+    for (int i = 0; i <= a.length(); i++) {
+        _states[i][b.size()] = a.length()-i;
+    }
+    /// pad outside, where a is empty
+    for (int i = 0; i <= b.length(); i++) {
+        _states[a.size()][i] = b.length()-i;
+    }
+    for (int i = a.size()-1; i >= 0; i--) {
+        for (int j = b.size()-1; j >= 0; j--) {
+            if (a[i] == b[j]) {
+                _states[i][j] = _states[i+1][j+1];
+            }
+            else{
+                _states[i][j] = min(_states[i+1][j+1], min(_states[i+1][j], _states[i][j+1])) + 1;
+            }
+        }
+    }
+    return _states[0][0];
+}
+
+bool Solution::wordBreak(string target, vector<string> &words){
+    unordered_set<string> _lookup(words.begin(), words.end());
+    if (target.empty() and _lookup.find("") == _lookup.end()) {
+        return false;
+    }
+    // setup
+    vector<bool> _states(target.size(), false);
+    for (int i = 0; i < target.size(); i++) {
+        if (_lookup.find(target.substr(0, i+1)) != _lookup.end()) {
+            _states[i] = true;
+        }
+        else{
+            // search j = 0->i-1
+            for (int j = 0; j < i and !_states[i]; j++) {
+                if (_states[j] and _lookup.find(target.substr(j+1, i-j)) != _lookup.end()) {
+                    _states[i] = true;
+                }
+            }
+        }
+    }
+    return _states.back();
+}
+
 const vector<pair<string, string>> _testcasesString = {
     {
         "abcdavidefg",
@@ -161,6 +214,20 @@ _testcasesSequence = {
     {
         "ABCD", "AEBD",
     }
+},
+_testcasesEdit = {
+    {
+        "abc", "dbca"
+    },
+    {
+        "horizon", "horizontal"
+    },
+    {
+        "dbca", "bca"
+    },
+    {
+        "edcba", "abcde",
+    }
 };
 vector<string> _testcasesPalindrome = {
     "AABCDEBAZ",
@@ -170,6 +237,20 @@ vector<string> _testcasesPalindrome = {
 vector<vector<int>> _testcasesLIS = {
     { 1, 12, 7, 0, 23, 11, 52, 31, 61, 69, 70, 2 },
     {1, 5, 2, 0, -1, 3,  8}
+};
+vector<pair<string, vector<string>>> _testcasesWordBreak = {
+    {
+        "IamSumit", {"I" , "have", "Jain", "Sumit", "am", "this", "dog"}
+    },
+    {
+        "thisisadog", {"I" , "have", "Jain", "Sumit", "am", "this", "dog"}
+    },
+    {
+        "thisisdog", {"I", "is" , "have", "Jain", "Sumit", "am", "this", "dog"}
+    },
+    {
+        "thisdoghavethis", {"I" , "have", "Jain", "Sumit", "am", "this", "dog"}
+    }
 };
 int main(){
     Solution solve;
@@ -182,8 +263,15 @@ int main(){
 //    for (string s : _testcasesPalindrome) {
 //        cout << solve.longestPalindromicSubsequence(s) << "\n";
 //    }
-    for (vector<int> test : _testcasesLIS) {
-        cout << solve.longestIncreasingSubsequence(test) << "\n";
+//    for (vector<int> test : _testcasesLIS) {
+//        cout << solve.longestIncreasingSubsequence(test) << "\n";
+//    }
+//    for (auto test : _testcasesEdit) {
+//        cout << solve.editDistance(test.first, test.second) << "\n";
+//    }
+    cout << boolalpha;
+    for (auto test : _testcasesWordBreak) {
+        cout << solve.wordBreak(test.first, test.second) << "\n";
     }
     return 0;
 }
