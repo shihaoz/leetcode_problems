f3c7abde2be319dd63d78b4a2b87f3c5e36c40f5
DavidZhang <shihaoz@umich.edu>
2016-09-17 13:49:45 -0400

    rod cutting

---
+++ rod_cutting.hpp
@@ -0,0 +1,14 @@
+//
+//  rod_cutting.hpp
+//  leetcode2016
+//
+//  Created by Shihao Zhang on 9/16/16.
+//  Copyright © 2016 David Zhang. All rights reserved.
+//
+
+#ifndef rod_cutting_hpp
+#define rod_cutting_hpp
+
+#include "common.h"
+
+#endif /* rod_cutting_hpp */
